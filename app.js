887d8e275b03e64c3cda720445f892d165059803
ThompsonCody <c.williamthompson@gmail.com>
2017-12-09 22:37:07 +0000

    big push, added more routes, cookies, README, and starting styling

--- app.js
+++ app.js
@@ -10,18 +10,27 @@ When we update a resource in an idempotent way we should use PUT, otherwise when
 If we are requesting to delete a resource, we should use DELETE.
 */
 
-const express     = require('express'),
-      bodyParser  = require('body-parser');
+const express       = require('express'),
+      bodyParser    = require('body-parser'),
+      cookieSession = require('cookie-session'),
+      bcrypt        = require('bcrypt');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 
 //--- Pug template engine ---
 app.set('view engine', 'pug');
-// app.set('view engine', 'ejs');
 
-//--- Body Parser ---
+//--- Passwords ---
+const saltRounds = 10;
+
+//--- Middlewares---
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['Cody'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 //--- Data ---
 let urlDB = {
@@ -32,6 +41,18 @@ let urlDB = {
   '9sm5xK': {
     longURL: 'http://www.google.com',
     userID: '2',
+  },
+};
+let usersDB = {
+  1: {
+    id: '1',
+    email: 'user@example.com',
+    password: 'p',
+  },
+  2: {
+    id: '2',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
   },
 };
 
@@ -47,76 +68,238 @@ var generateRandomString = () => {
     text += charset.charAt(Math.floor(Math.random() * charset.length));
   }
   return text;
-}
+};
 
+var emailTaken = (email) => {
+  let flag = false;
+  for(let id in usersDB){
+    if(usersDB[id].email === email) {
+      flag = true;
+    }
+  }
+};
+
+var findUser = (email) => {
+  for(let id in usersDB){
+    if(usersDB[id].email === email){
+      return usersDB[id];
+    }
+  }
+};
+
+var urlsForUser = (id, allURLS) => {
+  let myURLs = {};
+  for (let shortURL in allURLS) {
+    if (allURLS[shortURL].userID === id) {
+      myURLs[shortURL] = allURLS[shortURL].longURL;
+    }
+  }
+  return myURLs;
+}
 
 /*------ ****** ROUTES ****** ------*/
 
 
 // --- root ---
 app.get('/', (req, res) => {
-  res.end("HARI OM");
-});
-
-// --- home ---
-app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDB};
-  res.render('urls_index', templateVars);
-
-  console.log(templateVars);
-});
-
-// --- urls_new ---
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-  // - form handler
-  app.post("/urls", (req, res) => {
-    console.log('--Urls_New POST params:--\n', req.body); // debug statement to see POST params
-
-    let shortURL = generateRandomString();
-
-    //add the shortURL to DB with value of 'longURL: thisURL' object
-    urlDB[shortURL] = {longURL: req.body.longURL};
-
-    res.redirect(`http://localhost:8080/urls/${shortURL}`);
-  });
-
-// --- urls_show ---
-app.get('/urls/:id', (req, res) => {
-  let {id}    = req.params,
-      {longURL} = urlDB[id];
-  let templateVars = {
-    shortURL: id,
-    longURL: longURL
-  };
-  console.log(req.params);
-  res.render("urls_show", templateVars);
-});
-// - form handler
-app.post('/urls/:id/update', (req, res) =>{
-  let {id} = req.params;
-  urlDB[id].longURL = longURL;
   res.redirect("/urls");
 });
 
-// --- shortURL req, res to longURL ---
-app.get('/u/:shortURL', (req, res) => {
-  const { shortURL } = req.params;
-  const { longURL } = urlDB[shortURL];
+
+// --- home ---
+app.get("/urls", (req, res) => {
+  const user = usersDB[req.session["user_id"]];
+  if(!req.session.user_id){
+    res.status(404).redirect("/login");
+  } else {
+    let templateVars = {
+      urls: urlsForUser(req.session.user_id, urlDB),
+      user: usersDB[req.session.user_id]
+    };
+    res.render('urls_index', templateVars);
+  }
+});
+
+
+
+// --- REGISTER ---
+app.get('/registration', (req, res) => {
+  if (req.session.user_id) {
+    res.redirect('/urls');
+    return;
+  }
+
+  let templateVars = {urls: urlDB};
+  res.render('urls_registration', templateVars);
+});
+
+
+app.post('/registration', (req, res) => {
+  let{username, email, password} = req.body;
+    // console.log('Register body -->', username, email, password);
+
+  if (!(username || password || email)) {
+    res.status(400).render('urls_registration', {
+      error: 'Email or password missing'
+    });
+  }
+
+  if (emailTaken(email)) {
+    // console.log('Email Taken', usersDB);
+    res.status(400).render('urls_register', {
+      error: 'Email already here, but not here here, somewhere there'
+    });
+  } else {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      const randomId = generateRandomString();
+      const newUser =  {
+        id: randomId,
+        username,
+        email,
+        password: hash
+      };
+      usersDB[randomId] = newUser;
+
+      req.session.user_id = randomId;
+      res.redirect('/urls');
+    });
+  }
+});
+
+
+
+// --- LOGIN ---
+app.get('/login', (req, res) => {
+  if (req.session.user_id) {
+    res.redirect('/urls');
+    return;
+  } else {
+    res.render('urls_login');
+  }
+});
+app.post('/login', (req, res) => {
+  let {email, password} = req.body;
+  let user = findUser(email);
+  if (!user) {
+    res.status(403).render("urls_login", {
+      error: "User not found"
+    });
+  } else {
+    if (!bcrypt.compareSync(password, user.password)) {
+      res.status(403).render("urls_login", {
+        error: "Incorrect email or password"
+      });
+    } else {
+      req.session.user_id = user.id;
+      res.redirect('/urls');
+    }
+  }
+});
+
+//--- LOGOUT ---
+app.post('/urls/logout', (req, res) =>{
+  req.session = null;
+  res.redirect('/urls');
+});
+
+
+// --- urls_new ---
+app.get("/urls/new", (req, res) => {
+  if(!req.session.user_id){
+    res.redirect("/login");
+  } else {
+    let templateVars = {
+      urls: urlDB,
+      user: usersDB[req.session.user_id]
+    };
+    res.render("urls_new", templateVars);
+  }
+});
+  // - form handler for urls_new
+  app.post("/urls", (req, res) => {
+    // console.log('--Urls_New POST params:--\n', req.body); // debug statement to see POST params
+
+    if(!req.session.user_id){
+      res.status(404).send('404:Not Found');
+      return;
+    }
+    let shortURL = generateRandomString();
+
+    //add the shortURL to DB with value of 'longURL: thisURL' object
+    urlDB[shortURL] = {
+      longURL: req.body.longURL,
+      userID: req.session.user_id
+    };
+
+    res.redirect(`/urls`);
+  });
+
+
+// --- urls_show this shortURL ---
+app.get('/urls/:id', (req, res) => {
+  const user = usersDB[req.session["user_id"]];
+  if (user) {
+    let {id} = req.params;
+    let {longURL} = urlDB[id];
+
+    let templateVars = {
+      urls: urlDB,
+      user: user,
+      shortURL: id,
+      longURL: longURL
+    };
+    console.log('lookie -->', templateVars);
+    res.render("urls_show", templateVars)
+  } else {
+      res.status(401).render("urls_index", {error: 'You must be logged IN'}).redirect("/urls");
+  }
+});
+  // - form handler
+    //-- update url
+app.post('/urls/:id', (req, res) => {
+  if (!(req.params.id in urlDB)) {
+    res.status(404).send('404: Not found');
+    return;
+  }
+  if (req.session.user_id === urlDB[req.params.id].userID) {
+    urlDB[req.params.id].longURL = req.body.longURL;
+    res.redirect('/urls');
+  } else {
+    res.send('That is not yours!');
+  }
+});
+
+
+// --- shortURL req, res redirect to longURL ---
+app.get('/u/:id', (req, res) => {
+  if (!(req.params.id in urlDB)) {
+    res.status(404).send('404: Not found');
+    return;
+  }
+  const { id } = req.params;
+  const { longURL } = urlDB[id];
 
   res.redirect(longURL);
 });
 
 // --- remove url ---
-app.post('/urls/:url/delete', (req, res) => {
-  let {url} = req.params;
-  delete urlDB[url];
+app.post('/urls/:id/delete', (req, res) => {
+  const userID = req.session.user_id;
 
-  console.log(this);
-  res.redirect("/urls");
+  let {id}      = req.params,
+      urlObject = urlDB[id];
+
+  if (userID && usersDB[userID]){
+    if (userID === urlObject.userID){
+      delete urlDB[id];
+      res.redirect("/urls");
+    } else {
+      res.status(401).render("urls_index", {error: `you must be the owner of this URL`}).redirect("/urls");
+    }
+  } else {
+    res.status(401).render("urls_index", {error: `Not yours, shoo`}).redirect("/urls");
+  }
 });
-
 
 
 /*------ ****** PORT Listen ****** ------*/
